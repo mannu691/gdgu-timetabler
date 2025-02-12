4f23db2f9bc304351d224413d1702cf22e207e1f
mannug <mannugarg295@gmail.com>
2025-02-12 21:54:34 +0530

    fixed /database routing

--- src/main.ts
+++ src/main.ts
@@ -15,15 +15,14 @@ GlobalWorkerOptions.workerSrc = '/pdf.worker.mjs'
 export const timetableDB = new TimetableDB(TimetableType.Firestore)
 
 const app = createApp(App)
-
 app.use(router)
 app.use(PrimeVue, {
   theme: {
     preset: Aura,
     options: {
-        darkModeSelector: '.app-dark'
+      darkModeSelector: '.app-dark'
     }
-}
+  }
 })
 app.use(VueFire, {
   firebaseApp,

0e282df6eb61c597b69c061e2b731480858c3e1b
mannug <mannugarg295@gmail.com>
2025-02-12 22:02:28 +0530

    UI hot fix

--- src/components/Spinner.vue
+++ src/components/Spinner.vue
@@ -1,10 +1,12 @@
 <template>
-  <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+  <div class="fill-primary">
+    <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
 
-    <path d="M12,1A11,11,0,1,0,23,12,11,11,0,0,0,12,1Zm0,20a9,9,0,1,1,9-9A9,9,0,0,1,12,21Z" />
-    <rect class="spinner_d9Sa spinner_qQQY" x="11" y="6" rx="1" width="2" height="7" />
-    <rect class="spinner_d9Sa spinner_pote" x="11" y="11" rx="1" width="2" height="9" />
-  </svg>
+      <path d="M12,1A11,11,0,1,0,23,12,11,11,0,0,0,12,1Zm0,20a9,9,0,1,1,9-9A9,9,0,0,1,12,21Z" />
+      <rect class="spinner_d9Sa spinner_qQQY" x="11" y="6" rx="1" width="2" height="7" />
+      <rect class="spinner_d9Sa spinner_pote" x="11" y="11" rx="1" width="2" height="9" />
+    </svg>
+  </div>
 </template>
 <style scoped>
 .spinner_d9Sa {
